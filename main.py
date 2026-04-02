3d0847657eead57db14c29707b48d28161a848ea
kikuchi-rfp <kikuchi10bz@gmail.com>
2026-04-02 15:24:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,9 @@ from datetime import datetime
 from typing import Optional
 from flask import Flask, request, abort
 
-from linebot import WebhookHandler
-from linebot.v3.client import LineBotApi
-from linebot.v3.models import MessageEvent, TextMessage, TextSendMessage
-from linebot.v3.exceptions import InvalidSignatureError
+from linebot import Bot, WebhookHandler
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.exceptions import InvalidSignatureError
 
 from google.oauth2.service_account import Credentials
 from google.auth.transport.requests import Request
