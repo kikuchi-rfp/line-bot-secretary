43d72c2e67a78947e76bdbed47d66b6db7bec721
kikuchi-rfp <kikuchi10bz@gmail.com>
2026-04-02 15:40:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from datetime import datetime
 from typing import Optional
 from flask import Flask, request, abort
 
-from linebot import Bot, WebhookHandler
+from linebot import WebhookHandler
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from linebot.exceptions import InvalidSignatureError
 
